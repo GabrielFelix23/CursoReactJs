f0dfd05ab2af7b0979a1ed41f37909a6ae764baa
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-06 02:21:58 -0300

    props e component pt2

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
-const BemVindo = (props) => {
+const Equipe = (props) => {
     return(
         <div>
-            <h2>Bem vindo(a) {props.nome}</h2>
-            <h3>Tenho {props.idade} anos</h3>
+            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+
+            <Social fb={props.facebook}/>
+            <hr/>
+        </div>
+    )
+}
+
+const Sobre = (props) => {
+    return(
+        <div>
+            <h2>Olá sou o(a) {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade}</h3>
+        </div>
+    )
+}
+
+const Social = (props) => {
+    return(
+        <div>
+            <a href={props.fb}>Facebook</a><br/>
+            <a>Instagram</a><br/>
+            <a>Youtube</a>
         </div>
     )
 }
@@ -12,10 +34,19 @@ const BemVindo = (props) => {
 function App(){
     return(
         <div>
-            Olá mundo!
-            <BemVindo nome={"Gabriel"} idade={"32"}/>
-            <BemVindo nome={"Felix"} idade={"22"}/>
-            <h1>Curso de React</h1>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe 
+                nome="Gabriel" 
+                cargo="Programador" 
+                idade="22"
+                facebook="https://google.com"
+            />
+            <Equipe 
+                nome="Felix" 
+                cargo="Engenheiro" 
+                idade="30"
+                facebook="https://google.com"
+            />
         </div>
     )
 }
