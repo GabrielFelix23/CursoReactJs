2a95e35437520c338a4673e623723fd692f7be95
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-06 02:03:44 -0300

    props e component

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
+const BemVindo = (props) => {
+    return(
+        <div>
+            <h2>Bem vindo(a) {props.nome}</h2>
+            <h3>Tenho {props.idade} anos</h3>
+        </div>
+    )
+}
+
 function App(){
     return(
-        <h1>Olá mundo!</h1>
+        <div>
+            Olá mundo!
+            <BemVindo nome={"Gabriel"} idade={"32"}/>
+            <BemVindo nome={"Felix"} idade={"22"}/>
+            <h1>Curso de React</h1>
+        </div>
     )
 }
 
