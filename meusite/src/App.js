ed8b9fb9f904a51b4043284f15629788bf71dc1f
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-06 02:41:33 -0300

    Class component

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,34 +1,25 @@
 import React from 'react'
 
-const Equipe = (props) => {
-    return(
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
-
-            <Social fb={props.facebook}/>
-            <hr/>
-        </div>
-    )
+class Equipe extends React.Component{
+    render(){
+        return(
+            <div>
+                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+            </div>
+        )
+    }
 }
 
-const Sobre = (props) => {
-    return(
-        <div>
-            <h2>Olá sou o(a) {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade}</h3>
-        </div>
-    )
-}
-
-const Social = (props) => {
-    return(
-        <div>
-            <a href={props.fb}>Facebook</a><br/>
-            <a>Instagram</a><br/>
-            <a>Youtube</a>
-        </div>
-    )
+class Sobre extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Olá sou o {this.props.nome}</h2>
+                <h2>Cargo: {this.props.cargo}</h2>
+                <h2>Idade: {this.props.idade}</h2>
+            </div>
+        )
+    }
 }
 
 function App(){
@@ -39,13 +30,11 @@ function App(){
                 nome="Gabriel" 
                 cargo="Programador" 
                 idade="22"
-                facebook="https://google.com"
             />
             <Equipe 
                 nome="Felix" 
                 cargo="Engenheiro" 
                 idade="30"
-                facebook="https://google.com"
             />
         </div>
     )
