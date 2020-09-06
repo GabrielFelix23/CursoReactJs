728ab35653d1ee33f173ac650f4111cff6c3e9dd
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-06 03:04:00 -0300

    Trabalhando com states

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,43 +1,43 @@
 import React from 'react'
 
-class Equipe extends React.Component{
+class App extends React.Component{
+    state = {
+        estado: "",
+        contador: 0
+    }
+
+    aumentar = this.aumentar.bind(this)
+
+    aumentar(){
+        this.setState({
+            contador: this.state.contador += 1,
+            estado: "Aumentando"
+        })
+    }
+
+    diminuir = this.diminuir.bind(this)
+
+    diminuir(){
+        this.setState({
+            contador: this.state.contador -+ 1,
+            estado: "Diminuindo"
+        })
+    }
+
     render(){
         return(
             <div>
-                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+                <h1>Contador</h1>
+
+                <h3>
+                    <h5>{this.state.estado}</h5>
+                    <button onClick={this.diminuir}>-</button> 
+                    {this.state.contador} 
+                    <button onClick={this.aumentar}>+</button>
+                </h3>
             </div>
         )
     }
-}
-
-class Sobre extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>Olá sou o {this.props.nome}</h2>
-                <h2>Cargo: {this.props.cargo}</h2>
-                <h2>Idade: {this.props.idade}</h2>
-            </div>
-        )
-    }
-}
-
-function App(){
-    return(
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Equipe 
-                nome="Gabriel" 
-                cargo="Programador" 
-                idade="22"
-            />
-            <Equipe 
-                nome="Felix" 
-                cargo="Engenheiro" 
-                idade="30"
-            />
-        </div>
-    )
 }
 
 export default App
