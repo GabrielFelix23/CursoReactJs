7db97883898abc7446de8132b47d25df7a32c9a7
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-08 21:13:40 -0300

    Trabalhando com Listas

--- meusite/src/component/Membros/index.js
+++ meusite/src/component/Membros/index.js
@@ -1,36 +1,29 @@
 import React from 'react'
 
+import Feed from '../Feed/index'
+
 class Membros extends React.Component{
     state = {
-        status: true
-    }
-
-    sair = this.sair.bind(this)
-    
-    sair(){
-        this.setState({status: false})
-    }
-
-    entrar = this.entrar.bind(this)
-
-    entrar(){
-        this.setState({status: true})
+       feed: [
+           {id: 1, username: "Gabriel", curtidas: 10, comentarios: 2},
+           {id: 2, username: "Felix", curtidas: 5, comentarios: 10},
+           {id: 3, username: "Silva", curtidas: 100, comentarios: 20},
+           {id: 4, username: "Ana", curtidas: 1, comentarios: 0}
+       ]
     }
 
     render(){
         return(
             <div>
-              {this.state.status ? 
-                <div>
-                    <h2>Bem vindo ao sistema</h2>
-                    <button onClick={this.sair}>Sair</button>
-                </div> 
-                :
-                <div>
-                    <h2>Olá visitante, faça o seu login</h2>
-                    <button onClick={this.entrar}>Entrar no sistema</button>
-                </div>
-              }
+                {this.state.feed.map((item) => {
+                    return(
+                        <Feed id={item.id} username={item.username}
+                            curtidas={item.curtidas} comentarios={item.comentarios}
+                        />
+                    )
+                })
+
+                }
             </div>
         )
     }
