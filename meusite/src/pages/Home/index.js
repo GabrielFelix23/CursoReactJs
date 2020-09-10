133a6751780ac00f31b1a9d301fa736e45175eda
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-10 16:22:16 -0300

    Projeto Filmes pt2

--- meusite/src/pages/Home/index.js
+++ meusite/src/pages/Home/index.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import './home.css'
 
 class Home extends React.Component{
+    state = {
+        filmes: []
+    }
+
+    componentDidMount(){
+        this.loadFilmes()
+    }
+
+    loadFilmes = this.loadFilmes.bind(this)
+    loadFilmes(){
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes'
+        fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+            this.setState({filmes: json})
+            console.log(json)
+        })
+    }
+
     render() {
         return (
-            <div>
-                Pagina Home
+            <div className="container">
+                <div className="lista-filmes">
+                    {this.state.filmes.map((filme) => {
+                        return(
+                            <article key={filme.id} className="filme">
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto} alt="capa"/>
+                                <Link to="/">Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
             </div>
         );
     }
