80174b98e5fff05833762bab98cace92e60b3304
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-15 15:19:53 -0300

    Aplicando Styled Components

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Routes from './routes'
+
+import { Container, Header, Titulo, BemVindo} from './style'
 
 class App extends React.Component{
 
@@ -9,12 +10,26 @@ class App extends React.Component{
 
     render(){
         return(
-            <div>
-                <Routes/>
-            </div>
+            <Container>
+                <Header>
+                    <Titulo>Projeto Styled</Titulo>
+                </Header>
+
+                <BemVindo cor="00ff00">Bem vindo</BemVindo>
+            </Container>
         )
     }
 }
 
 
 export default App
+
+/**
+    <div className="container">
+        <header className="header">
+            <a className="titulo">Projeto Styled</a>
+        </header>
+
+        <h1>Bem vindo ao sistema!</h1>
+    </div>
+ */
