795082a9142a2bf165baa1b24a6189962195898d
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-16 16:52:56 -0300

    Instalando e configurando

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import Routes from './routes'
+import firebase from 'firebase'
 
 class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+        let firebaseConfig = {
+            apiKey: "AIzaSyCYAZYyMLRVduH9wdYSJVgEihqL6yNUBZM",
+            authDomain: "reactapp-8a8b8.firebaseapp.com",
+            databaseURL: "https://reactapp-8a8b8.firebaseio.com",
+            projectId: "reactapp-8a8b8",
+            storageBucket: "reactapp-8a8b8.appspot.com",
+            messagingSenderId: "740651159400",
+            appId: "1:740651159400:web:3f44aae97c1b11ae0b10dd",
+            measurementId: "G-MH25SEM3PQ"
+          };
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
+        
+    }
 
     render(){
         return(
             <div>
-                <Routes/>
+                <h1>Funcionando!</h1>
             </div>
         )
     }
