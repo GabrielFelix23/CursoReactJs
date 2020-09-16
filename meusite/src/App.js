b7fb442dd2cd03bf8d2a0a26542e31a979410009
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-16 20:36:34 -0300

    chamando database

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -5,7 +5,9 @@ class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-
+            token: "Carregando",
+            nome: '',
+            idade: ''
         }
         let firebaseConfig = {
             apiKey: "AIzaSyCYAZYyMLRVduH9wdYSJVgEihqL6yNUBZM",
@@ -16,16 +18,39 @@ class App extends React.Component{
             messagingSenderId: "740651159400",
             appId: "1:740651159400:web:3f44aae97c1b11ae0b10dd",
             measurementId: "G-MH25SEM3PQ"
-          };
+        }
           // Initialize Firebase
-          firebase.initializeApp(firebaseConfig);
+          firebase.initializeApp(firebaseConfig)
         
+          /*Olheiro
+          firebase.database().ref('token').on('value', (snapshot) => {
+              let state = this.state
+              state.token = snapshot.val()
+              this.setState(state)
+          })*/
+
+          firebase.database().ref('token').once('value').then((snapshot) => {
+            let state = this.state
+            state.token = snapshot.val()
+            this.setState(state)
+          })
+
+          firebase.database().ref('usuarios').child(1).on('value', (snapshot) => {
+              let state = this.state
+              state.nome= snapshot.val().nome
+              state.nome= snapshot.val().idade
+              this.setState(state)
+          })
     }
+       
+    
 
     render(){
         return(
             <div>
-                <h1>Funcionando!</h1>
+                <h1>Token: {this.state.token}</h1>
+                <h1>Nome: {this.state.nome}</h1>
+                <h1>Idade: {this.state.idade}</h1>
             </div>
         )
     }
