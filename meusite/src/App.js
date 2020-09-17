c2bec7b279301625df6aae161baf58921a56ee48
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-17 15:47:42 -0300

    Usando a nossa Database

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,60 +1,57 @@
-import React from 'react'
-import firebase from 'firebase'
-
-class App extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            token: "Carregando",
-            nome: '',
-            idade: ''
+import React, {Component} from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
+ 
+export default class App extends Component {
+   constructor(props){
+ 
+     super(props);
+     this.state={
+       token:'carregando',
+       idade: '',
+       nome: ''
+     }
+ 
+     let firebaseConfig = {
+        apiKey: "AIzaSyBSHlhNc19IjPFszn-KV2hmPSpu9YO6PMY",
+        authDomain: "reactjs-3f777.firebaseapp.com",
+        databaseURL: "https://reactjs-3f777.firebaseio.com",
+        projectId: "reactjs-3f777",
+        storageBucket: "reactjs-3f777.appspot.com",
+        messagingSenderId: "564769499349",
+        appId: "1:564769499349:web:8ed1aef108cdedc1ba1a5c"
+      };
+      // Initialize Firebase
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
         }
-        let firebaseConfig = {
-            apiKey: "AIzaSyCYAZYyMLRVduH9wdYSJVgEihqL6yNUBZM",
-            authDomain: "reactapp-8a8b8.firebaseapp.com",
-            databaseURL: "https://reactapp-8a8b8.firebaseio.com",
-            projectId: "reactapp-8a8b8",
-            storageBucket: "reactapp-8a8b8.appspot.com",
-            messagingSenderId: "740651159400",
-            appId: "1:740651159400:web:3f44aae97c1b11ae0b10dd",
-            measurementId: "G-MH25SEM3PQ"
-        }
-          // Initialize Firebase
-          firebase.initializeApp(firebaseConfig)
-        
-          /*Olheiro
-          firebase.database().ref('token').on('value', (snapshot) => {
-              let state = this.state
-              state.token = snapshot.val()
-              this.setState(state)
-          })*/
-
-          firebase.database().ref('token').once('value').then((snapshot) => {
-            let state = this.state
-            state.token = snapshot.val()
-            this.setState(state)
-          })
-
-          firebase.database().ref('usuarios').child(1).on('value', (snapshot) => {
-              let state = this.state
-              state.nome= snapshot.val().nome
-              state.nome= snapshot.val().idade
-              this.setState(state)
-          })
-    }
-       
-    
-
-    render(){
-        return(
-            <div>
-                <h1>Token: {this.state.token}</h1>
-                <h1>Nome: {this.state.nome}</h1>
-                <h1>Idade: {this.state.idade}</h1>
-            </div>
-        )
-    }
+ 
 }
-
-
-export default App
+ 
+    componentDidMount(){
+        firebase.database().ref('token').on('value', (snapshot)=>{
+            let state = this.state;
+            state.token = snapshot.val()
+            this.setState(state);
+        })
+    }
+    componentDidMount(){
+        firebase.database().ref('usuarios').child(1).on('value', (snapshot)=>{
+            let state = this.state;
+            state.nome = snapshot.val().nome
+            state.idade = snapshot.val().idade
+            this.setState(state);
+        })
+    }
+   
+   render(){
+     const{ token, nome, idade } = this.state
+     return(
+     <div>
+       <h1>Token: {token}</h1>
+       <h1>Nome: {nome}</h1>
+       <h1>Idade: {idade}</h1>
+     </div>)
+   }
+ 
+  }
