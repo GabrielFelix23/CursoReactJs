4859d35ba3ba74f7bc8c264f0c88d5be8a43eb37
GabrielFelix23 <srgfelix123@gmail.com>
2020-09-23 14:12:40 -0300

    retomando o estudo de firebase

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import firebase from './firebase'
-
-import Home from './components/Home'
-import Header from './components/Header'
-import './global.css' 
+import React from 'react'
+import firebase from 'firebase'
 
 class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
 
-    state = {
-        firebaseInitialized: false
+        }
+        let firebaseConfig = {
+            apiKey: "AIzaSyBKEAiZ-XOIzqaHrxTRCKFV1dqTdauKo1Y",
+            authDomain: "reactapp-d6c0a.firebaseapp.com",
+            databaseURL: "https://reactapp-d6c0a.firebaseio.com",
+            projectId: "reactapp-d6c0a",
+            storageBucket: "reactapp-d6c0a.appspot.com",
+            messagingSenderId: "1051912974597",
+            appId: "1:1051912974597:web:23f14409402a9ff730c76c"
+          };
+          // Initialize Firebase
+          if(firebase.apps.length){
+                firebase.initializeApp(firebaseConfig);
+          }
+        
     }
 
-    componentDidMount(){
-        firebase.isInitialized().then((resultado) => {
-            //devolve o usuario
-            this.setState({firebaseInitialized: resultado})
-        })
-    }
-
-    render() {
-        return this.state.firebaseInitialized != false ? (
-            <BrowserRouter>
-            <Header/>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                </Switch>
-            </BrowserRouter>
-        ) : (
-            <h1>Carregado...</h1>
+    render(){
+        return(
+            <div>
+                <h1>Funcionando</h1>
+            </div>
         )
     }
 }
