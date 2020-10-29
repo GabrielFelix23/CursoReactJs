99f99b5d5ca58924a9ca303035dfd16d434139e3
GabrielFelix23 <srgfelix123@gmail.com>
2020-10-29 14:04:11 -0300

    Projeto Repos

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,42 +1,9 @@
-import React, {useState, useEffect, useMemo, useCallback} from 'react'
+import React from 'react'
+import Routes from './routes'
 
 function App(){
-
-  const [tarefas, setTarefas] = useState([])
-  const [input , setInput] = useState('')
-
-  useEffect(() => {
-    const tarefasStorange = localStorage.getItem('tarefas')
-
-    if(tarefasStorange){
-      setTarefas(JSON.parse(tarefasStorange))
-    }
-  }, [])
-
-  //did update
-  useEffect(() => {
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-  }, [tarefas])
-
-  const handleAdd = useCallback(() => {
-    setTarefas([...tarefas, input])
-    setInput('')
-  }, [tarefas, input])
-
-  const totoalTarefas = useMemo(() => tarefas.length, [tarefas])
-  
-  return (
-    <div>
-        <ul>
-          {tarefas.map((tarefa) => (
-            <li key={tarefa}>{tarefa}</li>
-          ))}
-        </ul>
-        <br/>
-        <strong>Você tem {totoalTarefas} tarefas</strong><br/>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-        <button type="button" onClick={handleAdd}>Adicionar</button>
-    </div>
+  return(
+    <Routes/>
   )
 }
 
