be9f021ede4072067d05af07573333b8295d7136
GabrielFelix23 <srgfelix123@gmail.com>
2020-11-03 14:29:24 -0300

    Buscando repos pt2

--- meusite/src/pages/main/index.js
+++ meusite/src/pages/main/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useCallback} from 'react'
-import {FaGithub, FaPlus} from 'react-icons/fa'
+import {FaGithub, FaPlus, FaSpinner} from 'react-icons/fa'
 import {Container, Form, SubmitButton} from './styles'
 
 import api from '../../services/api'
@@ -8,19 +8,28 @@ export default function Main(){
 
     const [newRepo, setNewRepo] = useState('')
     const [repositorios, setRepositorios] = useState([])
+    const [loading, setLoading] = useState(false)
 
     const handleSubmit = useCallback((e) => {
         e.preventDefault()
 
         async function submit(){
-            const response = await api.get(`repos/${newRepo}`)
+            setLoading(true)
+            try{
+                const response = await api.get(`repos/${newRepo}`)
 
-            const data = {
-                name: response.data.full_name,
+                const data = {
+                    name: response.data.full_name,
+                }
+    
+                setRepositorios([...repositorios, data])
+                setNewRepo('')
+
+            }catch(error){
+                console.log(error)
+            }finally{
+                setLoading(false)
             }
-
-            setRepositorios([...repositorios, data])
-            setNewRepo('')
         }
 
         submit()
@@ -42,8 +51,13 @@ export default function Main(){
                 <input type="text" placeholder="Adicionar Repositorios"     
                     value={newRepo} onChange={handleinputChange}/>
                 
-                <SubmitButton>
-                    <FaPlus color="#fff" size={14}/>
+                <SubmitButton loading={loading ? 1 : 0}>
+                    {loading ? (
+                        <FaSpinner color="#fff" size={14}/>
+                    ) : (
+                        <FaPlus color="#fff" size={14}/>
+                    )
+                    } 
                 </SubmitButton>
             </Form>
         </Container>
--- meusite/src/pages/main/styles.js
+++ meusite/src/pages/main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, {keyframes, css} from 'styled-components'
 
 export const Container = styled.div`
     max-width: 700px;
@@ -32,9 +32,22 @@ export const Form = styled.form`
         font-size: 17px
     }
 `
-export const SubmitButton = styled.button.attrs({
-    type: 'submit'
-})`
+
+//criando animação do botao
+const animate = keyframes`
+    from{
+        transform: rotate(0deg)
+    }
+
+    to{
+        transform: rotate(360deg)
+    }
+`
+
+export const SubmitButton = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.loading
+}))`
     background: #0D2636;
     border: 0;
     border-radius: 4px;
@@ -43,5 +56,18 @@ export const SubmitButton = styled.button.attrs({
     
     display: flex;
     justify-content: center;
-    align-items: center
+    align-items: center;
+
+    &[disabled]{
+        cursor: not-allowed;
+        opacity: 0.5
+    }
+
+    ${props => props.loading &&
+        css`
+            svg{
+                animation: ${animate} 2s linear infinite
+            }
+        `
+    }
 `
