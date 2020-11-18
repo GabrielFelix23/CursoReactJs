92953f4f340f3ffdf20bf09aeb66120abc4f9dc8
GabrielFelix23 <srgfelix123@gmail.com>
2020-11-18 13:15:12 -0300

    Estilizando Home

--- meusite/src/pages/Home/index.js
+++ meusite/src/pages/Home/index.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react'
+import {MdFlightTakeoff} from 'react-icons/md'
+
 import api from '../../services/api'
+import './style.css'
 
 export default function Home(){
     const [trips, setTrips] = useState([])
@@ -8,14 +11,25 @@ export default function Home(){
         async function loadApi(){
             const response = await api.get('trips')
             setTrips(response.data)
-
-            console.log(response.data)
         }
         loadApi()
     }, [])
     return(
-        <div>
-            <h1>Tela Home</h1>
+        <div className="box">
+            {trips.map(trip => (
+                <li key={trips.id}>
+                     <img src={trip.image} alt={trip.title}/>
+                     <strong>{trip.title}</strong>
+                     <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+
+                     <button type="button" onClick={() => {}}>
+                         <div>
+                             <MdFlightTakeoff size={16} color={'#fff'}/>
+                         </div>
+                         <span>SOLICITAR RESERVA</span>
+                     </button>
+                </li>
+            ))}
         </div>
     )
 }
