70c938c48e9c48fa522e137ce41f9a0459ce62bd
GabrielFelix23 <srgfelix123@gmail.com>
2020-11-18 13:34:51 -0300

    Estilizando Reservas

--- meusite/src/pages/Reservas/index.js
+++ meusite/src/pages/Reservas/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './style.css'
+import {MdDelete} from 'react-icons/md'
 
 export default function Reserva(){
     return(
         <div>
-            <h1>Tela Reserva</h1>
+            <h1 className="title">Você solicitou 1 reserva</h1>
+
+            <div className="reservas">
+                <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+                alt="Maceio"/>
+                
+                <strong>Viagem Maceio 7 Dias</strong>
+                <strong>Quantidade: 2</strong>
+                
+                <button type="button" onClick={() => {}}>
+                    <MdDelete size={20} color="#000"/>
+                </button> 
+            </div>
+
+            <footer>
+                <button type="button">Solicitar Reservas</button>
+            </footer>
         </div>
     )
 }
