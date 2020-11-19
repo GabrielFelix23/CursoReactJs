50923e50b6c08a4ea7a426ef0606681e040dcb7a
GabrielFelix23 <srgfelix123@gmail.com>
2020-11-19 16:05:28 -0300

    Solicitando uma reserva

--- meusite/src/pages/Home/index.js
+++ meusite/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react'
+import {useDispatch} from 'react-redux'
 import {MdFlightTakeoff} from 'react-icons/md'
 
 import api from '../../services/api'
 import './style.css'
 
 export default function Home(){
+    const dispatch = useDispatch()
     const [trips, setTrips] = useState([])
 
     useEffect(() => {
@@ -13,7 +15,14 @@ export default function Home(){
             setTrips(response.data)
         }
         loadApi()
-    }, [])
+    }, []) 
+
+    function handleAdd(trip){
+        dispatch({
+            type:'ADD_RESERVE',
+            trip
+        })
+    }
     return(
         <div className="box">
             {trips.map(trip => (
@@ -22,7 +31,7 @@ export default function Home(){
                      <strong>{trip.title}</strong>
                      <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
 
-                     <button type="button" onClick={() => {}}>
+                     <button type="button" onClick={() => handleAdd(trip)}>
                          <div>
                              <MdFlightTakeoff size={16} color={'#fff'}/>
                          </div>
