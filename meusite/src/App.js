33f033215771151cac2c059d1f25b14a45a54345
GabrielFelix23 <srgfelix123@gmail.com>
2020-12-07 14:21:03 -0300

    useCallback

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useMemo} from 'react'
+import React, {useState, useEffect, useMemo, useCallback} from 'react'
 
 function App(){
   const [tarefas, setTarefas] = useState([])
@@ -18,10 +18,10 @@ function App(){
   }, [tarefas])
   
   
-  function handleAdd(){
-    setTarefas([...tarefas, input])
-    setInput('')
-  }
+  const handleAdd = useCallback(() => {
+  setTarefas([...tarefas, input])
+  setInput('')
+  }, [input, tarefas])
 
   const totalTarefas = useMemo(() => tarefas.length, [tarefas])
 
